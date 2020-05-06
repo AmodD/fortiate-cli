fea03d625094f2f8e2ee5d98e8b20971e8b9f8d4
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2020-05-06 16:18:00 +0530

    rapid-development fixed issue with spacing in commit message

--- git.js
+++ git.js
@@ -3,12 +3,12 @@ module.exports = {
   commands: function(program, shell) {
 
     program
-    .command('git <command> [action]')
+    .command('git <command> [action...]')
     .description('git commands shortcuts')
     .action((command, action) => {
       if (command === 'status') shell.exec('git status');
       if (command === 'add') shell.exec('git add .');
-      if (command === 'commit') shell.exec('git commit -m "' + action + '"');
+      if (command === 'commit') shell.exec('git commit -m "' + action.toString().replace(/,/g, ' ') + '"');
     });
 
   },
