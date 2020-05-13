200315056ec1cfcf5c23dedb796a8f4f2450b810
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2020-05-13 16:51:59 +0530

    TVKAN-52 #comment linting fixes

--- build.js
+++ build.js
@@ -13,14 +13,14 @@ module.exports = {
       if (what === 'all') console.log(logSymbols.info, 'Not implemented yet');
       else if (what === 'core') {
 
-        shell.cd("/usr/local/lib/node_modules/fortiate/core");
-        shell.exec('rm -rf fpf')
-        shell.exec("git clone git@github.com:fortiate/fpf.git",{silent: true});
-        shell.exec('docker rmi php-fortiate',{silent: true});
-        shell.exec('docker rmi python-fortiate',{silent: true});
+        shell.cd('/usr/local/lib/node_modules/fortiate/core');
+        shell.exec('rm -rf fpf');
+        shell.exec('git clone git@github.com:fortiate/fpf.git', {silent: true});
+        shell.exec('docker rmi php-fortiate', {silent: true});
+        shell.exec('docker rmi python-fortiate', {silent: true});
         shell.exec('docker build --file php-fortiate.docker -t php-fortiate .');
         shell.exec('docker build --file python-fortiate.docker -t python-fortiate .');
-        shell.exec('rm -rf fpf')
+        shell.exec('rm -rf fpf');
 
       } else console.log(logSymbols.info, 'Not implemented yet');
 
