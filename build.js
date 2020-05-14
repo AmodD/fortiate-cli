7adc38f2102ec7b95ee6d1a8ea907578e3e64d34
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2020-05-15 01:25:49 +0530

    rapid-development imagename instead of microservice

--- build.js
+++ build.js
@@ -10,9 +10,9 @@ module.exports = {
     .command('build <microservice> [deployment]')
     .description('builds source code')
     .action((microservice, deployment) => {
+
       if (microservice === 'all') console.log(logSymbols.info, 'Not implemented yet');
       else if (microservice === 'core') {
-
         shell.cd('/usr/local/lib/node_modules/fortiate/core');
         shell.exec('rm -rf fpf');
         shell.exec('git clone git@github.com:fortiate/fpf.git', {silent: true});
@@ -21,12 +21,16 @@ module.exports = {
         shell.exec('docker build --file php-fortiate.docker -t php-fortiate .');
         shell.exec('docker build --file python-fortiate.docker -t python-fortiate .');
         shell.exec('rm -rf fpf');
-
-      } else if (microservice !== '' && deployment !== '') {
+      } else if (microservice !== '' && typeof deployment !== 'undefined') {
         try {
           const fdeploypath = process.env.FORTIATE_HOME + '/' + 'deploy';
+          let imagename = microservice;
+
+          if (microservice === ('preprocessor' || 'cleaner' || 'labeller' || 'separator' || 'csv_to_rupay')) imagename = 'file-processors';
+          else if (microservice === 'rupay-packer') imagename = 'parser-packer';
+
           shell.cd(fdeploypath);
-          shell.exec('docker-compose -f docker-compose.' + deployment + '.yml build ' + microservice);
+          shell.exec('docker-compose -f docker-compose.' + deployment + '.yml build ' + imagename);
         } catch (err){
           console.error(err);
         }
