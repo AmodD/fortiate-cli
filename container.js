ca56357398e6d3ea285c22f14bbae902895b1ed9
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2020-05-15 16:09:58 +0530

    rapid-development if multiple or conditions written in a different more correct way probably

--- container.js
+++ container.js
@@ -4,7 +4,11 @@ function get(microservice){
 
   let container = microservice;
 
-  if (microservice === ('preprocessor' || 'cleaner' || 'labeller' || 'separator' || 'csv_to_rupay')) container = 'file-processors';
+  if (microservice === 'preprocessor') container = 'file-processors';
+  else if (microservice === 'cleaner') container = 'file-processors';
+  else if (microservice === 'labeller') container = 'file-processors';
+  else if (microservice === 'separator') container = 'file-processors';
+  else if (microservice === 'csv_to_rupay') container = 'file-processors';
   else if (microservice === 'rupay-packer') container = 'parser-packer';
 
   return container;
