3061602fac4b50bb9ab37a7adb84801fee6e2c54
kavathekargayatri <gayatri@fortiate.com>
2020-05-18 13:18:58 +0530

    #TVKAN-66 added fileprocessor's microservices in dockerfiles.js

--- container.js
+++ container.js
@@ -4,12 +4,7 @@ function get(microservice){
 
   let container = microservice;
 
-  if (microservice === 'preprocessor') container = 'file-processors';
-  else if (microservice === 'cleaner') container = 'file-processors';
-  else if (microservice === 'labeller') container = 'file-processors';
-  else if (microservice === 'separator') container = 'file-processors';
-  else if (microservice === 'csv_to_rupay') container = 'file-processors';
-  else if (microservice === 'rupay-packer') container = 'parser-packer';
+  if (microservice === 'rupay-packer') container = 'parser-packer';
 
   return container;
 
