aa1734cf70aa948089473860d5c6bed5f1b914fb
kavathekargayatri <gayatri@fortiate.com>
2020-05-22 15:39:43 +0530

    TVKAN-103

--- build.js
+++ build.js
@@ -19,7 +19,7 @@ module.exports = {
         if (typeof tag === 'undefined'){
           // shell.exec('docker rmi php-fortiate', {silent: true});
           // shell.exec('docker rmi python-fortiate', {silent: true});
-	  shell.cd(process.env.FORTIATE_HOME + '/build/workspaces/php-fortiate');
+          shell.cd(process.env.FORTIATE_HOME + '/build/workspaces/php-fortiate');
           shell.exec('docker build --file php-fortiate.docker -t php-fortiate .');
           shell.cd(process.env.FORTIATE_HOME + '/build/workspaces/fpf');
           shell.exec('docker build --file python-fortiate.docker -t python-fortiate .');
