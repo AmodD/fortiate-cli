48473677be592d0ac6932bec8667745cd77d3cc4
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2020-05-24 00:05:01 +0530

    rapid-development detached mode for docker-compose up

--- deploy.js
+++ deploy.js
@@ -37,13 +37,17 @@ module.exports = {
         const fdeploypath = process.env.FORTIATE_HOME + '/deploy';
         shell.cd(fdeploypath);
 
-        const cmd = 'docker-compose -f docker-compose.' + deployment + '.yml -p ' + deployment + ' up --build --force-recreate --no-deps';
+        const cmd = 'docker-compose -f docker-compose.' + deployment + '.yml -p ' + deployment + ' up -d --build --force-recreate --no-deps';
         const dcua = shell.exec(cmd);
 
         if (dcua.code !== 0) {
           console.error(dcua.stderr);
-          console.log(logSymbols.error, deployment);
+          console.log(logSymbols.error, deployment + " deployment failed");
           process.exit(1);
+        }
+        else {
+          console.log(logSymbols.success, deployment + " deployed");
+          process.exit(0);
         }
 
       } else if (container !== '' && deployment !== ''){
@@ -51,14 +55,18 @@ module.exports = {
 
         shell.cd(fdeploypath);
         const cmd1 = 'docker-compose -f docker-compose.' + deployment + '.yml -p ' + deployment;
-        const cmd2 = ' up --build --force-recreate --no-deps ' + container;
+        const cmd2 = ' up -d --build --force-recreate --no-deps ' + container;
         const cmd = cmd1 + cmd2;
         const dcum = shell.exec(cmd);
 
         if (dcum.code !== 0) {
           console.error(dcum.stderr);
-          console.log(logSymbols.error, container);
+          console.log(logSymbols.error, container + " deployment failed");
           process.exit(1);
+        }
+        else {
+          console.log(logSymbols.success, container + " deployed");
+          process.exit(0);
         }
 
       } else console.log(logSymbols.info, 'Not implemented yet');
