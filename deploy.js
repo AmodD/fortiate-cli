7f50d81fcb1a4240dac26e971c181a241ff296b9
kavathekargayatri <gayatri@fortiate.com>
2020-05-26 20:35:50 +0530

    CLI-1

--- deploy.js
+++ deploy.js
@@ -7,25 +7,26 @@ module.exports = {
     program
     .command('deploy <deployment> <container>') // exception fortiate deploy dev
     .description('deployment of fortiate containers')
-    .action((deployment, container) => {
+    .action(async(deployment, container) => {
 
       const fdeploypath = process.env.FORTIATE_HOME + '/deploy';
       shell.cd(fdeploypath);
 
-      let cmd = 'docker-compose -f docker-compose.' + deployment + '.yml -p ' + deployment + ' ';
+      let cmdmain = 'docker-compose -f docker-compose.' + deployment + '.yml -p ' + deployment + ' ';
 
-      if (container === 'all') cmd = cmd + 'up -d';
-      else if (container === 'down') cmd = cmd + 'down';
-      else cmd = cmd + 'up -d --build --force-recreate --no-deps ' + container;
-
-      const dc = shell.exec(cmd);
+      const dc = await execdockercompose(cmdmain, container);
 
       if (dc.code !== 0) {
         console.error(dc.stderr);
         console.log(logSymbols.error, deployment + ' deployment failed');
         process.exit(1);
       } else {
-        console.log(logSymbols.success, deployment + ' deployed');
+        if (container !== 'down') console.log(logSymbols.success, deployment + ' deployed');
+
+        const ps = shell.exec(cmdmain + ' ps | grep alarm',{silent:true});
+
+        if (container === 'alarm' || ps.stdout.includes('alarm')) await seeding(cmdmain,deployment);
+
         process.exit(0);
       }
 
@@ -33,3 +34,26 @@ module.exports = {
   }, // eof
 
 };// eome
+
+async function seeding(cmdmain,deployment){
+  const cmd = cmdmain + 'exec -T alarm php artisan migrate:refresh --seed';
+  const seeding = shell.exec(cmd);
+  
+if (seeding.code !== 0){
+    console.error(seeding.stderr);
+    console.log(logSymbols.error, deployment + ' seeding failed');
+    process.exit(1);
+  } else {
+    console.log(logSymbols.success, deployment + ' seeding is done');
+  }
+}
+
+async function execdockercompose(cmdmain, container){
+  let cmd = '';
+  if (container === 'all') cmd = cmdmain + 'up -d';
+  else if (container === 'down') cmd = cmdmain + 'down';
+  else cmd = cmdmain + 'up -d --build --force-recreate --no-deps ' + container;
+
+  return shell.exec(cmd);
+
+}
