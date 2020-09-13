9efcb05fd56f3904a0e64c56463aee806e953260
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2020-09-13 15:37:41 +0530

    cli-31

--- dockerfiles.js
+++ dockerfiles.js
@@ -54,7 +54,7 @@ function getlist(microservice){
   else if (microservice === 'modelsdb') commandlist = ['--file Dockerfile -t models-seeded'];
   else if (microservice === 'rulesdb') commandlist = ['--file Dockerfile -t rules-seeded'];
   else if (microservice === 'satsdb') commandlist = ['--file Dockerfile -t sats-seeded'];
-  // else if (microservice === 'transactionsdb') commandlist = ['--file Dockerfile -t transactions-seeded'];
+  else if (microservice === 'transactionsdb') commandlist = ['--file Dockerfile -t transactions-seeded'];
 
   return commandlist;
 
