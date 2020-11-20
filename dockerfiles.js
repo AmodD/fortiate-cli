ac2cb32ddb2ae9596b93503f4345d365b6e20a9a
Aniket Katkar <aniketfortiate>
2020-11-20 16:18:02 +0530

    DEP-50

--- dockerfiles.js
+++ dockerfiles.js
@@ -63,7 +63,7 @@ function getlist(microservice){
   else if (microservice === 'api-gateway') commandlist = ['--file Dockerfile -t api-gateway'];
   else if (microservice === 'propensitydb') commandlist = ['--file Dockerfile -t propensity-seeded'];
   else if (microservice === 'propensity-dbservice') commandlist = ['--file Dockerfile -t propensity-dbservice'];
-  else if (microservice === 'setup-services') commandlist = ['--file Dockerfile -t setup-services'];
+  else if (microservice === 'setup-services') commandlist = ['--file app.docker -t ss', '--file web.docker -t ss-web-server''];
 
   return commandlist;
 
